240fa197c01ad8bc629a36470fa12fcab8cc4d2e
Your Name <you@example.com>
2023-04-07 22:14:12 +0200

    css

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,15 +4,26 @@ const MovieCard = ({movie}) => {
     const IMAGE_PATH = "https://image.tmdb.org/t/p/w500"
     console.log(movie)
     return (
-        <div className="card text_center bg-secendary mb-3">
-            {movie.poster_path ? <img src={`${IMAGE_PATH}${movie.poster_path}`} alt=""/>
+        <div className="card">
+            {movie.poster_path ? <img src={`${IMAGE_PATH}${movie.poster_path}`} alt="" className="poster"/>
 : null
     }
+    <div className="movie-details">
+        <div className="box">
+            <div className="movieTitle">
 <h5>{movie.title}</h5>
-<h5>{movie.original_title}</h5>
+<h6>{movie.original_title}</h6>
+</div>
+<div className="overview">
+<h3>Overview</h3>
+<div className="rating">{movie.vote_average}</div>
 <p>{movie.overview}</p>
-<p>{movie.popularity}</p>
+
+</div>
         </div>
+        </div>
+        </div>
+        
     );
 };
 
